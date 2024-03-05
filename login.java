3d287a7b2ea01c58adeef83f80603c8d97e215ea
root <root@ip-172-31-17-85.ec2.internal>
2024-03-05 01:17:01 +0000

    added login.java

--- login.java
+++ login.java
@@ -1,2 +1,11 @@
-lkjlkjllkjlkjlkjlkjlkj
-kjlkjlkjlkjlkjlkk
+Public class login
+{
+   public static void main()
+   {
+       int i = 0;
+       for (i=0;i<=10;i++)
+       {
+	    System.out Println("Number: "+i);
+       }
+   }
+}
